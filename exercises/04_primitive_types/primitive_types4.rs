347c822832c99a6e7bf7a416bce793e8e149c8a1
gantoho <i@ganto.icu>
2025-01-25 21:39:30 +0800

    fix: fixed some bugs

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,7 +1,6 @@
 fn main() {
     // You can optionally experiment here.
 }
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -10,7 +9,7 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
-
+        let nice_slice = &a[1..4];
         assert_eq!([2, 3, 4], nice_slice);
     }
 }
